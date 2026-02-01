18df8b3e5c9c846e46f4ff6f3a777df2ab076631
Svein Olav Andersen <svan@fhi.no>
2026-02-01 21:52:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using OpenAI.Chat ;
+using System.ClientModel;
+
+var lmStudioEndpoint = new Uri("http://localhost:1234/v1");
+
+// VIKTIG: må matche /v1/models -> id
+var model = "qwen2.5-7b-instruct";
+
+// LM Studio krever ofte ikke nøkkel, men SDK-en trenger en placeholder
+var credential = new ApiKeyCredential("lm-studio");
+
+var openAi = new OpenAIClient(credential, new OpenAIClientOptions
+{
+    Endpoint = lmStudioEndpoint
+});
+
+await Oppgave1(model, openAi);
+
+static async Task Oppgave1(string model, OpenAIClient openAi)
+{
+    OpenAI.Chat.ChatClient rawChatClient = openAi.GetChatClient(model);
+    IChatClient chatClient = rawChatClient.AsIChatClient();
+
+    AIAgent agent = new ChatClientAgent(
+        chatClient,
+        instructions: "Du er en hjelpsom assistent. Svar kort og vennlig.",
+        name: "HelloAgent"
+    );
+
+    var response = await agent.RunAsync("Si hei, og fortell én morsom fakta om Norge.");
+    Console.WriteLine(response.Text);
+}
+
+
